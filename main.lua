c6af41258749e32c6c64b4a133c23c10311b6a69
allov <frederik.charest@gmail.com>
2025-03-22 13:37:33 -0400

    Initialize global gameState in main.lua

--- main.lua
+++ main.lua
@@ -1,24 +1,56 @@
+-- Global scene manager
+sceneManager = require('src.sceneManager')
+
+-- Global game state
+gameState = {
+    currentEncounter = nil,
+    selectedChef = nil,
+    progress = {
+        level = 1,
+        score = 0,
+        encounters = {}
+    }
+}
+
+-- Keyboard handling
+local keyPressed = {}
+
 function love.load()
-    -- Initialize game state
+    -- Load all scenes
+    local scenes = {
+        mainMenu = require('src.scenes.mainMenu'),
+        chefSelect = require('src.scenes.chefSelect'),
+        provinceMap = require('src.scenes.provinceMap'),
+        encounter = require('src.scenes.encounter')
+    }
+
+    -- Add scenes to manager
+    for name, scene in pairs(scenes) do
+        sceneManager:add(name, scene)
+    end
+    
+    -- Start with main menu
+    sceneManager:switch('mainMenu')
 end
 
 function love.update(dt)
-    -- Update game state
+    sceneManager:update(dt)
+    
+    -- Reset keyboard state
+    keyPressed = {}
 end
 
 function love.draw()
-    -- Set color to white
-    love.graphics.setColor(1, 1, 1, 1)
-    
-    -- Draw "Hello World" in the middle of the screen
-    love.graphics.print(
-        "Hello World!",
-        love.graphics.getWidth() / 2,
-        love.graphics.getHeight() / 2,
-        0,
-        2,
-        2,
-        50,
-        15
-    )
+    sceneManager:draw()
 end
+
+function love.keypressed(key)
+    keyPressed[key] = true
+end
+
+-- Helper function for keyboard input
+function love.keyboard.wasPressed(key)
+    return keyPressed[key]
+end
+
+
